4044dc6f08bd990f686d209e964ebe3a261d0393
BPPSwebsite <87129876+BPPSwebsite@users.noreply.github.com>
2022-01-24 17:21:19 +0100

    Add files via upload

--- flappy.js
+++ flappy.js
@@ -30,8 +30,8 @@ var cTop = -(600-characterTop)
 function jump(){
     */
     var cTop = -(700-characterTop)
-    if((characterTop>710)||(characterTop<140)||((blockLeft<30)&&(blockLeft>-70)&&((cTop-35<holeTop)||(cTop>holeTop+215)))){
-        alert("Bozo lost XD, Your score was "+counter)
+    if((characterTop>770)||(characterTop<140)||((blockLeft<30)&&(blockLeft>-70)&&((cTop-35<holeTop)||(cTop>holeTop+215)))){
+        alert("Bozo lost XD, Your score was "+counter + characterTop)
         character.style.top = 200 + "px";
         counter=0;
     }
